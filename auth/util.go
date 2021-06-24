61492af04f07ae26f28140407ee1c0b53f117c85
Karmanyaah Malhotra <karmanyaahm@gmail.com>
2021-06-24 18:00:13 -0400

    Tools for Optional Auth API endpoints

--- auth/util.go
+++ auth/util.go
@@ -14,17 +14,27 @@ func RegisterAuthentication(ctx *gin.Context, user *model.User, userID uint, tok
 
 // GetUserID returns the user id which was previously registered by RegisterAuthentication.
 func GetUserID(ctx *gin.Context) uint {
-	user := ctx.MustGet("user").(*model.User)
-	if user == nil {
-		userID := ctx.MustGet("userid").(uint)
-		if userID == 0 {
+	id := TryGetUserID(ctx) 
+	if id == nil {
 			panic("token and user may not be null")
-		}
-		return userID
 	}
-
-	return user.ID
+	return *id
 }
+
+// TryGetUserID returns the user id or nil if one is not set.
+func TryGetUserID(ctx *gin.Context) *uint {
+       user := ctx.MustGet("user").(*model.User)
+       if user == nil {
+               userID := ctx.MustGet("userid").(uint)
+               if userID == 0 {
+                       return nil
+               }
+               return &userID
+       }
+
+       return &user.ID
+}
+ 
 
 // GetTokenID returns the tokenID.
 func GetTokenID(ctx *gin.Context) string {
